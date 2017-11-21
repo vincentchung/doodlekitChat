e16c05620792ce56fb0de39dcd1d162a6f5ef8a8
Vincent <vincentcw.chung@gmail.com>
2017-11-21 15:41:51 +0800

    init nodejs server

---
+++ controllers/users.js
@@ -0,0 +1,14 @@
+'use stric';
+
+module.exports = function(_){
+
+  return {
+    SetRouting: function(router){
+      router.get('/',this.indexPage)
+    },
+
+    indexPage: function(req,res){
+      return res.render('index',{test:'This is test'});
+    }
+  }
+}
---
+++ user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+  username: {type: String, unique: true},
+  fullname: {type: String, unique: true, default: ''},
+  email: {type: String, unique: true},
+  password: {type: String, default: ''},
+  userImage: {type: String, default: 'default.png'},
+  fbTokens: Array,
+  google: {type: String, default: ''},
+  googleTokens: Array
+});
+
+module.exports = mongoose.model('User',userSchema);
